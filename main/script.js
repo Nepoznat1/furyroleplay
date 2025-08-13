4735452ce2e3746b73947522a91bc0ed90d95b2c
Nepoznat1 <126260740+Nepoznat1@users.noreply.github.com>
2025-08-14 00:18:08 +0200

    Update script.js

--- main/script.js
+++ main/script.js
@@ -3,32 +3,27 @@ const vehicles = [
   {
     name: "BMW M5 F90",
     price: "20€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
   {
     name: "Brabus 6x6",
     price: "30€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
   {
     name: "Lamborghini Huracan",
     price: "25€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
   {
     name: "Tesla Model S",
     price: "15€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
   {
     name: "Ferrari F8",
     price: "35€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
 ]
 
@@ -36,38 +31,32 @@ const organizations = [
   {
     name: "Vagos",
     price: "50€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
   {
     name: "Ballas",
     price: "50€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
   {
     name: "Medelin Cartel",
     price: "60€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
   {
     name: "Los Santos Police",
     price: "40€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
   {
     name: "Grove Street",
     price: "45€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
   {
     name: "Owner",
     price: "1245€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
 ]
 
@@ -75,26 +64,22 @@ const admins = [
   {
     name: "Helper Admin",
     price: "10€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
   {
     name: "Moderator",
     price: "25€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
   {
     name: "Admin",
     price: "50€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
   {
     name: "Super Admin",
     price: "100€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
 ]
 
@@ -102,26 +87,22 @@ const commands = [
   {
     name: "VIP Komande",
     price: "15€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
   {
     name: "Premium Komande",
     price: "30€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
   {
     name: "Elite Komande",
     price: "50€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
   {
     name: "Ultimate Komande",
     price: "75€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
 ]
 
@@ -129,44 +110,40 @@ const addons = [
   {
     name: "Skin Pack",
     price: "5€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
   {
     name: "Weapon Pack",
     price: "12€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
   {
     name: "Map Pack",
     price: "20€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
   {
     name: "Complete Pack",
     price: "35€",
-    image:
-      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+    image: "public/logo.png",
   },
 ]
 
 const gameImages = [
-  "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
-  "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
-  "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
-  "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
-  "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
-  "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
-  "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+  "public/logo.png",
+  "public/logo.png",
+  "public/logo.png",
+  "public/logo.png",
+  "public/logo.png",
+  "public/logo.png",
+  "public/logo.png",
 ]
 
 // Function to create donation cards
 function createDonationCard(item, type) {
   return `
         <div class="donation-card">
-            <img src="${item.image}" alt="${item.name}" class="donation-image" loading="lazy" onerror="this.src='https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&'">
+            <img src="${item.image}" alt="${item.name}" class="donation-image" loading="lazy" onerror="this.src='public/logo.png'">
             <h4 class="donation-name">${item.name}</h4>
             <p class="donation-price">Cena: ${item.price}</p>
             <a href="https://discord.com/channels/1384976556748963901/1384976560083308573" 
@@ -183,7 +160,7 @@ function createDonationCard(item, type) {
 function createGalleryItem(image, index) {
   return `
         <div class="gallery-item">
-            <img src="${image}" alt="Fury RolePlay V2 screenshot ${index + 1}" class="gallery-image" loading="lazy" onerror="this.src='https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&'">
+            <img src="${image}" alt="Fury RolePlay V2 screenshot ${index + 1}" class="gallery-image" loading="lazy" onerror="this.src='public/logo.png'">
         </div>
     `
 }
