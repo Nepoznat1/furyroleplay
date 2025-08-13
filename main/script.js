b0ac161c539bcbbd8301d429f78bfb8ab2ed2635
Nepoznat1 <126260740+Nepoznat1@users.noreply.github.com>
2025-08-13 02:39:08 +0200

    Update script.js

--- main/script.js
+++ main/script.js
@@ -1,35 +1,90 @@
 // Data arrays
 const vehicles = [
-  { name: "BMW M5 F90", price: "20€", image: "public/bmw-m5-f90.png" },
-  { name: "Brabus 6x6", price: "30€", image: "public/brabus-6x6.png" },
-  { name: "Lamborghini Huracan", price: "25€", image: "public/lamborghini-huracan.png" },
-  { name: "Tesla Model S", price: "15€", image: "public/tesla-model-s.png" },
-  { name: "Ferrari F8", price: "35€", image: "public/ferrari-f8.png" },
+  {
+    name: "BMW M5 F90",
+    price: "20€",
+    image:
+      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+  },
+  {
+    name: "Brabus 6x6",
+    price: "30€",
+    image:
+      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+  },
+  {
+    name: "Lamborghini Huracan",
+    price: "25€",
+    image:
+      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+  },
+  {
+    name: "Tesla Model S",
+    price: "15€",
+    image:
+      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+  },
+  {
+    name: "Ferrari F8",
+    price: "35€",
+    image:
+      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+  },
 ]
 
 const organizations = [
-  { name: "Vagos", price: "50€", image: "public/vagos-logo.png" },
-  { name: "Ballas", price: "50€", image: "public/ballas-logo.png" },
-  { name: "Medelin Cartel", price: "60€", image: "public/medelin-cartel-logo.png" },
-  { name: "Los Santos Police", price: "40€", image: "public/lspd-logo.png" },
-  { name: "Grove Street", price: "45€", image: "public/grove-street-logo.png" },
-  { name: "Owner", price: "1245€", image: "public/owner-logo.png" },
+  {
+    name: "Vagos",
+    price: "50€",
+    image:
+      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+  },
+  {
+    name: "Ballas",
+    price: "50€",
+    image:
+      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+  },
+  {
+    name: "Medelin Cartel",
+    price: "60€",
+    image:
+      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+  },
+  {
+    name: "Los Santos Police",
+    price: "40€",
+    image:
+      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+  },
+  {
+    name: "Grove Street",
+    price: "45€",
+    image:
+      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+  },
+  {
+    name: "Owner",
+    price: "1245€",
+    image:
+      "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+  },
 ]
 
 const gameImages = [
-  "public/fivem-police-chase.png",
-  "public/fivem-luxury-cars-street-racing.png",
-  "public/fivem-gang-meeting.png",
-  "public/fivem-city-nightlife-roleplay.png",
-  "public/fivem-police-station.png",
-  "public/fivem-custom-vehicles.png",
+  "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+  "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+  "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+  "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+  "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
+  "https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&",
 ]
 
 // Function to create donation cards
 function createDonationCard(item, type) {
   return `
         <div class="donation-card">
-            <img src="${item.image}" alt="${item.name}" class="donation-image" loading="lazy" onerror="this.src='public/placeholder-p0265.png'">
+            <img src="${item.image}" alt="${item.name}" class="donation-image" loading="lazy" onerror="this.src='https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&'">
             <h4 class="donation-name">${item.name}</h4>
             <p class="donation-price">Cena: ${item.price}</p>
             <a href="https://discord.com/channels/1384976556748963901/1384976560083308573" 
@@ -46,7 +101,7 @@ function createDonationCard(item, type) {
 function createGalleryItem(image, index) {
   return `
         <div class="gallery-item">
-            <img src="${image}" alt="Fury RolePlay V2 screenshot ${index + 1}" class="gallery-image" loading="lazy" onerror="this.src='public/placeholder-p0265.png'">
+            <img src="${image}" alt="Fury RolePlay V2 screenshot ${index + 1}" class="gallery-image" loading="lazy" onerror="this.src='https://cdn.discordapp.com/attachments/1236605109962670181/1394432084709474444/image.png?ex=689d0514&is=689bb394&hm=4ae7f6fcddd0c11e9e09e6de99bcb7f6c980b918f9d14fa179ed45887cdf0f52&'">
         </div>
     `
 }
